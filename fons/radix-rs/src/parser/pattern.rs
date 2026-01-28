85121dc955ab13b86eb3cbff3dff043f8d25d412
Ian Zepp <ian.zepp@protonmail.com>
2026-01-27 23:39:49 -0500

    feat(radix): allow template literal patterns

--- fons/radix-rs/src/parser/pattern.rs
+++ fons/radix-rs/src/parser/pattern.rs
@@ -130,6 +130,11 @@ impl Parser {
                 self.advance();
                 Some(Pattern::Literal(Literal::String(sym), span))
             }
+            TokenKind::TemplateString(sym) => {
+                let span = self.peek().span;
+                self.advance();
+                Some(Pattern::Literal(Literal::TemplateString(sym), span))
+            }
             TokenKind::Verum => {
                 let span = self.peek().span;
                 self.advance();
